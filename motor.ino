353a94831bf0d221878b6442bd2136d73c1e7b62
Gabriel Levy <gabelevy@gmail.com>
2016-01-27 23:12:58 -0800

    PID

--- motor.ino
+++ motor.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_LEDBackpack.h>
+
 #include <Wire.h>
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
@@ -19,13 +21,15 @@ class Motor
       pinMode(start_stop_pin_, INPUT);
       pinMode(speed_in_pin_, INPUT);
       pinMode(speed_out_pin_, OUTPUT);
-      attachInterrupt(digitalPinToInterrupt(speed_in_pin_), speed_pulse, FALLING);
+      attachInterrupt(digitalPinToInterrupt(speed_in_pin_), pulse, FALLING);
       attachInterrupt(digitalPinToInterrupt(start_stop_pin_), start_stop, RISING);
+      timer_.begin(pid_sample, 100000);
     }
 
     ~Motor() {
       detachInterrupt(digitalPinToInterrupt(speed_in_pin_));
       detachInterrupt(digitalPinToInterrupt(start_stop_pin_));
+      timer_.end();
     }
 
     static unsigned int get_rpm() {
@@ -63,43 +67,59 @@ class Motor
       noInterrupts();
       if (target_speed_ == 0) {
         Serial.println("START");
-        target_speed_ = RUN_SPEED;
+        target_rpm_ = RUN_SPEED;
       } else {
         Serial.println("STOP");
-        target_speed_ = 0;        
+        target_rpm_ = 0;        
       }
       interrupts();
     }
 
-    static void speed_pulse() {
+    static void pulse() {
       noInterrupts();
-      unsigned long now = micros();
-      if (last_pulse_us_ == 0 || now < last_pulse_us_) {
-        last_pulse_us_ = now;
-        // first pulse or rollover = bad sample
-        interrupts();
-        return;
-      }
-      rpm_ = 15 * (1000000UL / (now - last_pulse_us_));      
-      last_pulse_us_ = now;
+      ++count;
       interrupts();
     }
 
+    static void pid_sample() {
+      noInterrupts();
+      int rpm = 15 * count_;
+      count_ = 0;
+      int error = rpm - target_rpm_;
+      error = rpm_
+      i_state_ += error;
+      i_state = max(min_i_, min(max_i_, i_state));
+      int step = min(max_step_, 
+        ((p_gain * error) + (i_gain * i_state_) - (d_gain * (rpm - rpm_))));
+      v_control_ += step;
+      rpm_ = rpm;
+      interrupts();
+    }
+
+    IntervalTimer timer_;
     int start_stop_pin_;
     int speed_in_pin_;
-    static int speed_out_pin_;
     
-    static volatile unsigned int target_speed_;
+    static int speed_out_pin_;
+    static volatile unsigned int target_rpm_;
+    static volatile int rpm_;
+    static volatile unsigned int count_;
     static volatile unsigned int v_control_;
-    static volatile unsigned long last_pulse_us_;
-    static volatile unsigned int rpm_;
+    static volatile int i_state_;
+    const static volatile int p_gain_;
+    const static volatile int i_gain_;
+    const static volatile int d_gain_;    
 };
 
 int Motor::speed_out_pin_;
-volatile unsigned int Motor::target_speed_ = 0;
+volatile unsigned int Motor::target_rpm_ = 0;
+volatile int Motor::rpm_ = 0;
+volatile unsigned int Motor::count_ = 0;
 volatile unsigned int Motor::v_control_ = 0;
-volatile unsigned long Motor::last_pulse_us_ = 0;
-volatile unsigned int Motor::rpm_ = 0;
+volatile int Motor::i_state_ = 0;
+const int Motor::p_gain_ = 0;
+const int Motor::i_gain_ = 0;
+const int Motor::d_gain_ = 0;    
 
 Motor motor(SPEED_IN_PIN, SPEED_OUT_PIN, START_STOP_PIN);
 Adafruit_AlphaNum4 lcd = Adafruit_AlphaNum4();
